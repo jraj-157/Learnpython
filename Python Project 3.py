63b1bb81e9250451b7feff5bd764215029b7ef6f
jraj patnaik <96097449+jraj-157@users.noreply.github.com>
2022-01-30 15:05:44 +0530

    Add files via upload

---
+++ Python Project 3.py
@@ -0,0 +1,17 @@
+# 1
+name = input("Enter your name:")
+print("Good afternoon"+" "+name)
+# 2
+name1 = input("Enter your name:")
+print(" ''' Dear "+" "+name1+" "+"you are selected!")
+# 3
+str = "jsg gsg    dgdg gg"
+print(str.count("  "))
+# 4
+str1 = "jsg gsg    dgdg gg"
+print(str1.replace("  "," "))
+# 5
+z = " \" Dear Jraj , You are really handsome ! \" "
+print(z)
+
+
