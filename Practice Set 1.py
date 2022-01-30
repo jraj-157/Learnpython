54ff748f530183e63dcf8f11994b202a4c2ecbaf
jraj patnaik <96097449+jraj-157@users.noreply.github.com>
2022-01-30 10:08:28 +0530

    Add files via upload

---
+++ Practice Set 1.py
@@ -0,0 +1,24 @@
+# 1
+print("Twinkle Twinkle Little Star")
+# 2
+print(5*1,5*2,5*3,5*4,5*5)
+# 3import os
+from playsound import playsound
+playsound('/Users/jagnyarajpatnaik/Downloads/Doobey Gehraiyaan 320 Kbps.mp3')
+# 4
+import os
+print(os.listdir())
+# 5
+# jraj_157
+# learned from code with Herry 
+# location berhampur
+import os
+print(os.listdir())
+
+
+
+
+
+
+
+
