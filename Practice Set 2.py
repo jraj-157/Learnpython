3cc9ab6cccd67e2c95c04e2123d635403e64c2af
jraj patnaik <96097449+jraj-157@users.noreply.github.com>
2022-01-30 12:06:47 +0530

    Add files via upload

---
+++ Practice Set 2.py
@@ -0,0 +1,25 @@
+# 1
+a = input("type first numver")
+b = input("type second number")
+print(int(a)+int(b))
+# 2
+c = input("type the number")
+print(int(c)%2)
+# 3
+# all inouts are taken as a string
+# 4
+d = input("put integer1:")
+e = input("put integer2:")
+print(int(d)>=int(e))
+# 5
+f = input("a:")
+g = input("b:")
+h = int(f)+int(g)
+print(int(h)/2)
+# 6
+from cmath import sqrt
+import math
+z = input("enter no.:")
+print(sqrt(int(z)))
+
+
