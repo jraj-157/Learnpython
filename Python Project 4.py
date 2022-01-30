b87d911d1dfdc9a969053d873e1157c815ac1016
jraj patnaik <96097449+jraj-157@users.noreply.github.com>
2022-01-30 15:47:43 +0530

    Add files via upload

---
+++ Python Project 4.py
@@ -0,0 +1,29 @@
+# # 1
+a = input("Enter 1st fruit:")
+b = input("Enter 2nd fruit:")
+c = input("Enter 3rd fruit:")
+d = [a,b,c]
+print(d)
+# # 2
+q = input("Math Mark:")
+w = input("Physics Mark:")
+e = input("Chemistry Mark:")
+z = [q,w,e]
+print(z)
+# 3
+v = (10,110,120)
+print(v.insert(0,1)) # AttributeError: 'tuple' object has no attribute 'insert'
+# 4
+m = input("Enter First Number:")
+n = input("Enter Second Number:")
+o = input("Enter Third Number:")
+p = input("Enter Fourth Number:")
+list = [m,n,o,p]
+print(list)
+print(int(m)+int(n)+int(o)+int(p))
+# 5
+f = (7,0,8,0,0,9)
+print(f.count(0))
+
+
+
