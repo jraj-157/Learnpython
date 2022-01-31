04d47c2e46cf8fa477b6ee7def2496cd5a9c1327
jraj patnaik <96097449+jraj-157@users.noreply.github.com>
2022-01-31 20:39:34 +0530

    Add files via upload

---
+++ Python Project 6.py
@@ -0,0 +1,65 @@
+# # 1
+a = input("Enter First No:")
+b = input("Enter Second No:")
+c = input("Enter Third No:")
+d = input("Enter Fourth No:")
+s = int(a),int(b),int(c),int(d)
+print(max(s))
+# 2
+math = input("Enter Marks:")
+physics = input("Enter Marks:")
+chemistry = input("Enter Marks:")
+if int(math) >= 33:
+       int(physics) >= 33
+       int(chemistry) >= 33
+       int(math)/3+int(physics)/3+int(chemistry)/3 >=40
+       print("pass")
+else:
+    print("failed")
+# 3
+o = input("enter cmt:")
+if("make a lot of money","click on this","noice" in o):
+    print("spam")
+else:
+    print("Nope")
+# 4
+v = input("Enter your Name:")
+if len(v) >= 10:
+    print("yes")
+else:
+    print("No")
+# 5
+z = input("Enter Text:")
+if ("ram" in z ):
+    print("Yes")
+else:
+    print("No")
+# 6
+mark = input("Enter Agregate Marks:")
+if  int(mark) >= 100:
+    print("Invalid")
+elif 90 <= int(mark) <= 100 :
+    print("Ex")
+elif 80 <= int(mark) < 90:
+    print("A")
+elif 70 <= int(mark) < 80:
+    print("B")
+elif 60 <= int(mark) < 70:
+    print("c")
+elif 50 <= int(mark) < 60:
+    print("D")
+else:
+    print("fail")
+# 7
+cmt = input("Enter Comment:")
+if cmt.__contains__("Jraj"):
+    print("yes")
+else:
+    print("no")
+
+
+
+
+
+
+     
