e62aad2c261a1d5424170e86b1e0030960210fb7
jraj patnaik <96097449+jraj-157@users.noreply.github.com>
2022-01-31 19:21:35 +0530

    Add files via upload

---
+++ Python Project 5.py
@@ -0,0 +1,39 @@
+# 1
+a = {"c*****":"F****",
+       "ma****":"MC",
+
+       }
+
+b = input("enter the word:")
+if b == "c*****":
+    print(a["c*****"])
+elif b == "ma****":
+ print(a["ma****"])
+else:
+    print("aukat ke bahar")
+# 2
+c = input("Enter First Number:")
+d = input("Enter Second Number:")
+e = input("Enter Third Number:")
+q = {c,d,e}
+print(q)
+# 3
+s = {1,5,7,"aa"}
+print(s)
+# 4
+s.add(20)
+s.add(20.0)
+s.add("20")
+print(s)
+# 5
+print(type(s))
+# 6
+a1 = input("enter your Fav.. language:")
+a2 = input("enter your Fav.. language:")
+a3 = input("enter your Fav.. language:")
+dic = {"ram":a1,
+         "syam":a2,
+         "dule":a3
+         }
+print(dic)
+
