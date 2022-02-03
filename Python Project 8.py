fd08490f195a88beea622b5d1b9d304c0c130119
jraj patnaik <96097449+jraj-157@users.noreply.github.com>
2022-02-03 11:11:59 +0530

    Add files via upload

---
+++ Python Project 8.py
@@ -0,0 +1,58 @@
+# 1
+# a = int(input("Enter NO:"))
+# b = int(input("Enter NO:"))
+# c = int(input("Enter NO:"))
+# def gtrt(a,b,c):
+#     if (a > b and a > c):
+#             return a
+#     elif  (b > c and b > a):
+#           return b
+#     else:
+#         return c
+
+# q = gtrt(a,b,c)
+
+# print(q)
+# 2
+# ce = int(input("Enter the celcius:"))
+# def conversion(ce):
+#     return  (1.8 * ce) + 32
+# f = conversion(ce)
+# print(f)
+# 3
+# def line(name):
+#     print(name+" Best Wishes")
+# line("Jraj")
+# 4
+# n = int(input("Enter No"))
+# def sum(n):
+#     return (n + 1)/2
+# def sum1(n):
+#     return n * sum(n)
+# q = sum1(n)
+# print(q)
+# 5
+# i = 4
+# while(i>0):
+#     i = i - 1
+#     print("*" * i )
+# 6
+# n = int(input("Enter Inches:"))
+# def fun_inch_to_cm(n):
+#     return n * 2.54
+# q = fun_inch_to_cm(n)
+# print(q)
+# 7
+word = input("Enter the word:")
+li = ["Maggie","Topremen","Yipee"]
+def trim(word):
+    return li.__delitem__(word)
+     
+
+
+
+
+
+
+
+
